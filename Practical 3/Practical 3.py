28cf105b2d51022efa66f4f0ed2f7882872b60d3
Tanvi Radia <97345560+20CS067@users.noreply.github.com>
2022-04-14 17:09:57 +0530

    Add files via upload

---
+++ Practical 3/Practical 3.py
@@ -0,0 +1,10 @@
+x = input()
+roomlist = input().split()
+roomset = set(roomlist)
+
+for y in list(roomset):
+    roomlist.remove(y)
+
+CAP_RO_NUM = roomset.difference(set(roomlist)).pop()
+print(CAP_RO_NUM)
+
